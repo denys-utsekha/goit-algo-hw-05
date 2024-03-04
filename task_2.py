b730bb8d04696a2b1c1235c61eab04aad63ae4e6
Denys Utsekha <denysutsekha@gmail.com>
2024-03-04 22:37:38 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,21 +2,25 @@ test_numbers = [0.1, 0.25, 0.5, 0.75, 1.0, 1.25,
                 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
 
 
-def binary_search(arr, x):
+def binary_search_with_upper_bound(arr, x):
     low = 0
     high = len(arr) - 1
     count = 0
+    upper_bound = arr[-1]
 
     while low <= high:
         mid = (low + high) // 2
         count += 1
-        if arr[mid] == x:
-            return (count, arr[mid])
-        if arr[mid] > x:
+
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            upper_bound = arr[mid]
             high = mid - 1
         else:
-            low = mid + 1
-    return -1
+            return count, arr[mid]
+
+    return count, upper_bound
 
 
-print(binary_search(test_numbers, 1.75))
+print(binary_search_with_upper_bound(test_numbers, 0.26))
